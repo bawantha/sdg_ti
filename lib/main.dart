b18d200c1c871e0f8774afa4d4f01bcafd5d9eeb
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2024-03-27 00:43:16 +0530

    add common packages with localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sdg_ti/config/extensions/context_extension.dart';
 
 void main() {
+  Bloc.observer = const AppBlocObserver();
   runApp(const SDGApp());
+}
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    if (bloc is Cubit) debugPrint(change.toString());
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    debugPrint(transition.toString());
+  }
 }
 
 class SDGApp extends StatelessWidget {
@@ -11,17 +34,28 @@ class SDGApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SDG App'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SDG App'),
+      ),
+      body: Center(
+        child: Text(context.localizations.helloWorld),
       ),
     );
   }
